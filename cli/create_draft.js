c5cc3be7e7750057d7908d1deb4294aaf01e034a
Derek Rose <bishma@gmail.com>
2016-03-18 16:52:19 -0700

    start on a script to create new draft seasons

---
+++ cli/create_draft.js
@@ -0,0 +1,93 @@
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: '../data/draft.nedb', autoload: true });
+var prompt = require('prompt');
+
+// this governs the user prompts and valid responses
+var schema = {
+    properties: {
+        season: {
+            description: "Season",
+            validator: /^(summer|winter)$/i,
+            warning: '"summer" or "winter"',
+            required: true
+        },
+        year: {
+            description: "Year (YYYY)",
+            validator: /^20\d{2}$/,
+            warning: "Four digit year only",
+            required: true
+        },
+        draft_start: {
+            description: "Drafting Starts (YYYYMMDD)",
+            validator: /^20\d{6}$/,
+            warning: "YYYYMMDD format.",
+            required: true
+        },
+        draft_end: {
+            description: "Drafting Ends (YYYYMMDD)",
+            validator: /^20\d{6}$/,
+            warning: "YYYYMMDD format.",
+            required: true
+        },
+        season_start: {
+            description: "Season Starts (YYYYMMDD)",
+            validator: /^20\d{6}$/,
+            warning: "YYYYMMDD format.",
+            required: true
+        },
+        season_end: {
+            description: "Season Ends (YYYYMMDD)",
+            validator: /^20\d{6}$/,
+            warning: "YYYYMMDD format.",
+            required: true
+        }
+    }
+};
+
+prompt.start();
+
+// welocome the people
+console.log("Hello and welcome to new draft setup. We'll just need to answer a few questions\n");
+
+prompt.get(schema, function(err,result) {
+    // @todo err to error.log
+    if (err) { console.log("Unable to get prompt response",err); process.exit(1); };
+
+    // lets see if the specified draft exists
+    db.find({ season: result.season, year: result.year }, function (err, docs) {
+        if (err) { console.log("Unable to get search database",err); process.exit(1); };
+
+        // add a movies array which we'll populate in another script
+        result.movies = [];
+
+        // check to see if there's aleady a draft for this season and year
+        if (docs.length != 0) {
+            console.log("There is already a draft for this season and year. Overwrite (and lose movie list) (y/N)");
+
+            // prompt to see if we should overwrite the existing season information
+            prompt.get(['overwrite'], function (err,ynresp) {
+                if (err) { console.log("Unable to get search database",err); process.exit(1); };
+
+                // if we got a yes prompt then add the ID we found so the DB knows to replace instead of add new
+                if (ynresp.overwrite === 'y' || ynresp.overwrite === 'Y') {
+                    result['_id'] = docs['_id'];
+                    db.insert(result,function(err,resp) {
+                        if (err) { console.log("Unable to get insert new draft",err); process.exit(1); };
+        
+                        console.log("Draft replaced. God speed. You'll need it.");
+                    });
+                } else {
+                    // if we got something other than a yes response then we stop insertion
+                    console.log("Draft creation halted.");
+                }
+            });
+        } else {
+            db.insert(result,function(err,resp) {
+                if (err) { console.log("Unable to get insert new draft",err); process.exit(1); };
+        
+                console.log("Draft created. All hail George Lucas, king of the pizza buffet.");
+            });
+        }
+    });
+});
+
