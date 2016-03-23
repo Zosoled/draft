80da8b88738c8c5a4f3566e6039dc42f6b78192e
Derek Rose <bishma@gmail.com>
2016-03-22 21:13:16 -0700

    fixed formatting for jade each loop

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var Datastore = require('nedb')
+  , db = new Datastore({ filename: 'data/draft.nedb', autoload: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var movies= [];
+
+    db.find({}, function(err, docs) {
+        if (err) { console.log("Unable to get draft documents",err); process.exit(1); };
+console.log(docs);
+        //if (docs.length !== 1) { console.log("Found an inappropriate number of documents: "+docs.length); process.exit(1); };
+
+        movies = docs.movies;
+    });
+    res.render('index', { title: 'IDX Movie Draft', movies: movies });
 });
 
 module.exports = router;
