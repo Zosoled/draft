e21d7862f2d43a035799700c6f3ca3a5bd3e02f2
Derek Rose <bishma@gmail.com>
2016-03-29 13:23:04 -0700

    first stab at dynamic data loading through helper functions

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var helper = require(global.appRoot + '/modules/helpers.js');
+var Datastore = require('nedb');
+var db = new Object;
 db.draft = new Datastore({ filename: 'data/draft.nedb', autoload: true });
 db.movie = new Datastore({ filename: 'data/movie.nedb', autoload: true });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    var current_draft = helper.currentDraft();
+    if (typeof current_draft == 'object') {
+        var selection_draft = current_draft;
+    }
+console.log(selection_draft);
 
-    db['movie'].find({ season: 'summer', year: '2016' }).sort({ release_date: 1 }).exec( function(err, docs) {
+    db['movie'].find( selection_draft ).sort({ release_date: 1 }).exec( function(err, docs) {
         if (err) { console.log("Unable to get movie documents",err); process.exit(1); };
 
         res.render('index', { title: 'IDX Movie Draft', movies: docs });
