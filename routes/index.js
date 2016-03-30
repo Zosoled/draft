99418b6e82efb822c925cd57906eccb721739c13
Derek Rose <bishma@gmail.com>
2016-03-29 20:06:57 -0700

    added more dynamic content to pages

--- routes/index.js
+++ routes/index.js
@@ -12,19 +12,31 @@ router.get('/', function(req, res, next) {
     if (typeof current_draft == 'object') {
         var selection_draft = current_draft;
     }
-console.log(selection_draft);
 
     db['movie'].find( selection_draft ).sort({ release_date: 1 }).exec( function(err, docs) {
         if (err) { console.log("Unable to get movie documents",err); process.exit(1); };
 
-        res.render('index', { title: 'IDX Movie Draft', movies: docs });
+        // get the draft details as well
+        db['draft'].findOne( selection_draft ).exec(function(err, doc) {
+            if (err) { console.log("Unable to get movie documents",err); process.exit(1); };
+            selection_draft.draft_start = doc.draft_start;
+            selection_draft.draft_end = doc.draft_end;
+            selection_draft.season_start = doc.season_start;
+            selection_draft.season_end = doc.season_end;
+
+            res.render('index', { title: 'IDX Movie Draft', movies: docs, current_draft: selection_draft });
+        });
     });
 });
 
 /* team addtions page */
 router.get('/add_team', function(req, res, next) {
+    var current_draft = helper.currentDraft();
+    if (typeof current_draft == 'object') {
+        var selection_draft = current_draft;
+    }
 
-    res.render('add_team', {title: 'Add a drafting team' });
+    res.render('add_team', {title: 'Add a drafting team', current_draft: selection_draft});
 });
 
 module.exports = router;
