e16de410bb6bb78397ee6b194066a473d37aceb0
Derek Rose <bishma@gmail.com>
2016-03-30 16:16:14 -0700

    added actual db insert to the add team route

--- routes/index.js
+++ routes/index.js
@@ -13,25 +13,24 @@ router.get('/', function(req, res, next) {
         var selection_draft = current_draft;
     }
 
-    db['movie'].find( selection_draft ).sort({ release_date: 1 }).exec( function(err, docs) {
+    db['movie'].find( selection_draft ).sort({ release_date: 1 }).exec( function(err, movie_docs) {
         if (err) { console.log("Unable to get movie documents",err); process.exit(1); };
 
         // get the draft details as well
-        db['draft'].findOne( selection_draft ).exec(function(err, doc) {
+        db['draft'].findOne( selection_draft ).exec(function(err, draft_doc) {
             if (err) { console.log("Unable to get movie documents",err); process.exit(1); };
-            selection_draft.draft_start = doc.draft_start;
-            selection_draft.draft_end = doc.draft_end;
-            selection_draft.season_start = doc.season_start;
-            selection_draft.season_end = doc.season_end;
+            selection_draft.draft_start = draft_doc.draft_start;
+            selection_draft.draft_end = draft_doc.draft_end;
+            selection_draft.season_start = draft_doc.season_start;
+            selection_draft.season_end = draft_doc.season_end;
 
-            res.render('index', { title: 'IDX Movie Draft', movies: docs, current_draft: selection_draft });
+            res.render('index', { title: 'IDX Movie Draft', movies: movie_docs, current_draft: selection_draft });
         });
     });
 });
 
 // team addtions page
 router.get('/add_team', function(req, res, next) {
-    console.log(req.query);
     var current_draft = helper.currentDraft();
     if (typeof current_draft == 'object') {
         var selection_draft = current_draft;
@@ -53,14 +52,24 @@ router.post('/add_team', function(req, res, next) {
         'team_name',
         'member_name_1',
         'member_name_2',
-        'member_name_2'
+        'member_name_3'
     ].forEach(function (elm) {
         if (typeof req.body[elm] != "string" || req.body[elm].length == 0) {
             res.status(200).redirect("/add_team?required");
         }
     });
 
-    res.status(200).redirect("/");
+    // check to see if name is taken
+
+    req.body._id = req.body.season + "-" + req.body.year + "-" + req.body.team_name.replace(/\s+/g,"_").replace(/![a-zA-Z0-9_]/,"");
+
+    db.team = new Datastore({ filename: 'data/team.nedb', autoload: true });
+    db['team'].insert(req.body, function(err) {
+        if (err) { console.log("Unable to insert into team db.",err); process.exit(1); }
+
+        res.status(200).redirect("/");
+    });
+
 });
 
 module.exports = router;
