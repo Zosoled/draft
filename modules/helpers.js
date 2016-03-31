ebe56eaa0e7613feb57f9d038254b723dd97188f
Derek Rose <bishma@gmail.com>
2016-03-31 13:17:52 -0700

    added function to auto create safe IDs

--- modules/helpers.js
+++ modules/helpers.js
@@ -10,4 +10,19 @@ helpers.currentDraft = function() {
     return { season: "summer", year: "2016" };
 }
 
+// take an array, make them id/url friendly, then concat with a hyphen
+helpers.makeID = function(thing) {
+    if (typeof thing == "string") {
+        thing = [ thing ];
+    }
+    else if (!Array.isArray(thing)) {
+        return false;
+    }
+
+    for (var i = 0; i < thing.length; i++) {
+        thing[i] = thing[i].replace(/\s+/g,"_").replace(/[^a-zA-Z0-9_]/,"");
+    }
+    return thing.join('-');
+}
+
 module.exports = helpers;
