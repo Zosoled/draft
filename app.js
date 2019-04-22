76dc274aaba413d50feade41620665a0a8ee62d3
Chris Duncan <chris@theduncans.us>
2019-04-22 00:18:28 -0700

    Add pug (new version of deprecated jade). Removed sprintf (deprecated). Updated dependency versions.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sprintf = require('sprintf');
 
 // figure out the root path and set it global
 var path = require('path');
@@ -16,7 +15,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
