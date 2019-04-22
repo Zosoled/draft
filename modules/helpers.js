4d83aef04bd7577165a64220df299925e4b27ae2
Chris Duncan <chris@theduncans.us>
2019-04-22 00:27:00 -0700

    Update current season.

--- modules/helpers.js
+++ modules/helpers.js
@@ -7,7 +7,7 @@ helpers.currentDate = function() {
 
 // get the current draft
 helpers.currentDraft = function() {
-    return { season: "summer", year: "2018" };
+    return { season: "summer", year: "2019" };
 }
 
 // take an array, make them id/url friendly, then concat with a hyphen
