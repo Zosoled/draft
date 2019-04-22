5e883f68e3578208f42a24dfead111cf0871f138
Chris Duncan <chris@theduncans.us>
2019-04-21 19:34:21 -0700

    Remove unused and deprecated sprintf library.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sprintf = require('sprintf');
 
 // figure out the root path and set it global
 var path = require('path');
