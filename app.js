d6f0ae85fbedfe16e7a2d49a69837d704ca50812
Chris Duncan <chris@theduncans.us>
2019-05-21 23:01:40 -0700

    Tab align dependencies.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express			= require('express');
+var path			= require('path');
+var favicon			= require('serve-favicon');
+var logger			= require('morgan');
+var cookieParser	= require('cookie-parser');
+var bodyParser		= require('body-parser');
 
 // figure out the root path and set it global
 global.appRoot = path.resolve(__dirname);
