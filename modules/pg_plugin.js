54c1ff186d7ac9e52f8c44537d3b2530f2d5924e
Chris Duncan <chris@theduncans.us>
2020-05-02 21:21:26 -0700

    Formatting fixup.

--- modules/pg_plugin.js
+++ modules/pg_plugin.js
@@ -1,114 +1,110 @@
 /* Initialize Postgres database */
-let pg_user = 'jcfdytkfjfekdz';
-let pg_pw = 'dd9e14615e59b7153495f9d4aee8e1e512d0855d15610f5e67c7d003c2d6c41d';
-let pg_host = 'ec2-54-225-242-183.compute-1.amazonaws.com';
-let pg_port = '5432';
-let pg_db = 'dftp19h0fcneq7';
-let pg_uri = 'postgres://' + pg_user + ':' + pg_pw + '@' + pg_host + ':' + pg_port + '/' + pg_db;
-const { Pool, Client } = require('pg');
+const pgUser = 'jcfdytkfjfekdz'
+const pgPassword = 'dd9e14615e59b7153495f9d4aee8e1e512d0855d15610f5e67c7d003c2d6c41d'
+const pgHost = 'ec2-54-225-242-183.compute-1.amazonaws.com'
+const pgPort = '5432'
+const pgDatabase = 'dftp19h0fcneq7'
+const pgUri = 'postgres://' + pgUser + ':' + pgPassword + '@' + pgHost + ':' + pgPort + '/' + pgDatabase
+const { Pool } = require('pg')
 const pool = new Pool({
-	connectionString: process.env.DATABASE_URL || pg_uri,
-	ssl: true
-});
+  connectionString: process.env.DATABASE_URL || pgUri,
+  ssl: true
+})
 
 /* Build table creation query strings */
-let pg_schema = new Array();
-pg_schema.push(
-	{
-		name: 'draft',
-		create: 'CREATE TABLE draft(' +
-			'id				char(16)	primary key,' +
-			'season			text,' +
-			'year			int,' +
-			'draft_start	date,' +
-			'draft_end		date,' +
-			'season_start	date,' +
-			'season_end		date' +
-		');'
-	},
-	{
-		name: 'movie',
-		create: 'CREATE TABLE movie(' +
-			'id				char(16)	primary key,' +
-			'draft_id		char(16)	references Draft(id),' +
-			'name			text,' +
-			'release_date	date,' +
-			'bom_id 		text,' +
-			'imdb_id 		text,' +
-			'poster_url		text,' +
-			'yt_id			text,' +
-			'done			bool' +
-		');'
-	},
-	{
-		name: 'player',
-		create: 'CREATE TABLE player(' +
-			'id			char(16)	primary key,' +
-			'movies		movie[],' +
-			'name		text' +
-		');'
-	},
-	{
-		name: 'team',
-		create: 'CREATE TABLE team(' +
-			'id					char(16)	primary key,' +
-			'draft_id			char(16)	references Draft(id),' +
-			'players			player[],' +
-			'name				text,' +
-			'draft_position		int,' +
-			'draft_complete		bool' +
-		');'
-	}
-);
-insertTables(pg_schema);
+const pgSchema = [{
+  name: 'draft',
+  create: 'CREATE TABLE draft(' +
+    'id char(16) primary key,' +
+    'season text,' +
+    'year int,' +
+    'draftStart date,' +
+    'draftEnd date,' +
+    'seasonStart date,' +
+    'seasonEnd date' +
+  ');'
+},
+{
+  name: 'movie',
+  create: 'CREATE TABLE movie(' +
+    'id char(16) primary key,' +
+    'draftId char(16) references Draft(id),' +
+    'name text,' +
+    'releaseDate date,' +
+    'imdbId text,' +
+    'posterUrl text,' +
+    'youtubeId text,' +
+    'done bool' +
+  ');'
+},
+{
+  name: 'player',
+  create: 'CREATE TABLE player(' +
+    'id char(16) primary key,' +
+    'movies movie[],' +
+    'name text' +
+  ');'
+},
+{
+  name: 'team',
+  create: 'CREATE TABLE team(' +
+    'id char(16) primary key,' +
+    'draftId char(16) references Draft(id),' +
+    'players player[],' +
+    'name text,' +
+    'draftPosition int,' +
+    'draftComplete bool' +
+  ');'
+}
+]
+insertTables(pgSchema)
 
-
-async function insertTables(pg_schema) {
-	let currentTables = await getCurrentTables();
-	for (let i=0; i<pg_schema.length; i++) {
-		let found = false;
-		for (let i=0; i<currentTables.length; i++) {
-			if (currentTables[i].tablename == pg_schema[i].name) {
-				found = true;
-			}
-		}
-		if ( ! found ) {
-			insertTable(pg_schema[i].create);
-		}
-	}
+async function insertTables (pgSchema) {
+  const currentTables = await getCurrentTables()
+  for (let i = 0; i < pgSchema.length; i++) {
+    let found = false
+    for (let i = 0; i < currentTables.length && !found; i++) {
+      if (currentTables[i].tablename === pgSchema[i].name) {
+        found = true
+      }
+    }
+    if (!found) {
+      insertTable(pgSchema[i].create)
+    }
+  }
 }
 
-async function insertTable(tableSchema) {
-	const client = await pool.connect();
-	try {
-		await client.query('BEGIN');
-		await client.query(tableSchema);
-		await client.query('COMMIT');
-	} catch (e) {
-		await client.query('ROLLBACK');
-		console.log(e);
-		throw e;
-	} finally {
-		client.release()
-	}
+async function insertTable (tableSchema) {
+  const client = await pool.connect()
+  try {
+    await client.query('BEGIN')
+    await client.query(tableSchema)
+    await client.query('COMMIT')
+  } catch (e) {
+    await client.query('ROLLBACK')
+    console.log(e)
+    throw e
+  } finally {
+    client.release()
+  }
 }
 
-async function getCurrentTables() {
-	let client = await pool.connect();
-	let results = new Array();
-	try {
-		let query = {
-			text: 'SELECT * FROM pg_catalog.pg_tables WHERE tableowner != $1',
-			values: ['postgres']
-		}
-		let reply = await client.query(query);
-		results = (reply) ? reply.rows : results;
-	} catch (e) {
-		console.log(e);
-		throw e;
-	} finally {
-		client.release()
-	}
-	console.log(results);
-	return results;
+async function getCurrentTables () {
+  const client = await pool.connect()
+  let results = []
+  try {
+    const query = {
+      text: 'SELECT * FROM pg_catalog.pg_tables WHERE tableowner != $1',
+      values: ['postgres']
+    }
+    const reply = await client.query(query)
+    results = (reply) ? reply.rows : results
+  } catch (e) {
+    console.log(e)
+    throw e
+  } finally {
+    client.release()
+  }
+  console.log(results)
+  return results
 }
